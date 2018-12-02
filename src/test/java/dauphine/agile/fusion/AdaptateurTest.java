3ebf441708f4efee04fc0452ea971c316ee78bf4
Kévin Pang <kevinpg94310@gmail.com>
2018-12-02 18:11:28 +0100

    Ajout tests de pilotage des fusées avec les adaptateurs pour un padawan et un maitre

--- src/test/java/dauphine/agile/fusion/AdaptateurTest.java
+++ src/test/java/dauphine/agile/fusion/AdaptateurTest.java
@@ -4,36 +4,54 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import dauphine.agile.space.Astronaut;
+import dauphine.agile.space.EscapePod;
 import dauphine.agile.space.IAstronaut;
 import dauphine.agile.space.Rocket;
 import dauphine.agile.starwars.Maitre;
+import dauphine.agile.starwars.Padawan;
 
 public class AdaptateurTest {
 
 	@Test
 	public void insertionMaitreAvecCertificat() {
 
-		try {
+		Maitre yoda = new Maitre("Yoda", "Yoda", "Jedi");
 
-			Maitre yoda = new Maitre("Yoda", "Yoda", "Jedi");
-			// Yoda passe la formation
-			CentreDeCertification.delivrer(yoda);
+		// Yoda passe la formation
+		CentreDeCertification.delivrer(yoda);
+		Rocket xwing = new Rocket();
+		IAstronaut yodaAstronaut = CentreDeCertification.delivrer(yoda);
 
-			Rocket xwing = new Rocket();
-
-			IAstronaut yodaAstronaut = CentreDeCertification.delivrer(yoda);
-
-			xwing.enter(yodaAstronaut);
-			
-			
-			
-			
-
-		} catch (IllegalArgumentException e) {
-
-			assertTrue(false);
-		}
+		/**
+		 * test l'état de yodaAstronaut
+		 */
+		EscapePod escapePod = new EscapePod();
+		xwing.enter(yodaAstronaut);
+		xwing.setEscapePod(escapePod);
+		yodaAstronaut.piloter(xwing);
+		assertEquals("calm", yodaAstronaut.state());
 
 	}
 
+	@Test
+	public void insertionPadawanAvecCertificat() {
+
+		Padawan ashoka = new Padawan("Thano", "ashoka", "Jedi");
+
+		// Yoda passe la formation
+		CentreDeCertification.delivrer(ashoka);
+		Rocket xwing = new Rocket();
+		IAstronaut yodaAstronaut = CentreDeCertification.delivrer(ashoka);
+
+		/**
+		 * test l'état de yodaAstronaut
+		 */
+		EscapePod escapePod = new EscapePod();
+		xwing.enter(yodaAstronaut);
+		xwing.setEscapePod(escapePod);
+		yodaAstronaut.piloter(xwing);
+		assertEquals("calm", yodaAstronaut.state());
+
+	}
 }
